5e430836986b9b7cf644bdd1bfc0b21bd0026857
unknown <chris-winter@live.de>
2021-08-02 13:02:48 +0200

    setup the project

---
+++ TournamentReloaded/TournamentReloaded/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TournamentReloaded.Global" Language="C#" %>
---
+++ TournamentReloaded/TournamentReloaded/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace TournamentReloaded
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code, der beim Anwendungsstart ausgeführt wird
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            System.Web.Routing.RouteTable.Routes.MapPageRoute("Index", "", "~/Views/Gateway.aspx");
+            System.Web.Routing.RouteTable.Routes.MapPageRoute("Participants", "ParticipantManager", "~/Views/ParticipantManager.aspx");
+            System.Web.Routing.RouteTable.Routes.MapPageRoute("Teams", "TeamManager", "~/Views/TeamManager.aspx");
+            System.Web.Routing.RouteTable.Routes.MapPageRoute("Tournaments", "TournamentManager", "~/Views/TournamentManager.aspx");
+            System.Web.Routing.RouteTable.Routes.MapPageRoute("Play", "Play", "~/Views/PlayView.aspx");
+        }
+    }
+}
